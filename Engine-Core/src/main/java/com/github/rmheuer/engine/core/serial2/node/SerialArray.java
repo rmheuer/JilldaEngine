754ee6f10d827e726a4a3cb577f45e0102f35569
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-08-07 17:08:10 -0500

    Core: Add specific type methods to SerialObject and SerialArray

--- Engine-Core/src/main/java/com/github/rmheuer/engine/core/serial2/node/SerialArray.java
+++ Engine-Core/src/main/java/com/github/rmheuer/engine/core/serial2/node/SerialArray.java
@@ -12,10 +12,53 @@ public final class SerialArray extends TraversableNode implements Iterable<Seria
     }
 
     public void add(SerialNode node) {
+        if (node == null)
+            node = new SerialNull();
+
         int idx = list.size();
         list.add(node);
         node.setParent(this);
         node.setPathToken(Integer.toString(idx));
+    }
+
+    public void addByte(byte val) {
+        add(new SerialByte(val));
+    }
+
+    public void addShort(short val) {
+        add(new SerialShort(val));
+    }
+
+    public void addInt(int val) {
+        add(new SerialInt(val));
+    }
+
+    public void addLong(long val) {
+        add(new SerialLong(val));
+    }
+
+    public void addFloat(float val) {
+        add(new SerialFloat(val));
+    }
+
+    public void addDouble(double val) {
+        add(new SerialDouble(val));
+    }
+
+    public void addChar(char val) {
+        add(new SerialChar(val));
+    }
+
+    public void addString(String val) {
+        add(new SerialString(val));
+    }
+
+    public void addBoolean(boolean val) {
+        add(new SerialBoolean(val));
+    }
+
+    public void addReference(SerialNode target) {
+        add(new SerialRef(target));
     }
 
     public SerialNode get(int idx) {
@@ -28,6 +71,58 @@ public final class SerialArray extends TraversableNode implements Iterable<Seria
 
     public SerialNode getActual(int idx) {
         return list.get(idx);
+    }
+
+    public SerialObject getObject(int idx) {
+        return (SerialObject) get(idx);
+    }
+
+    public SerialArray getArray(int idx) {
+        return (SerialArray) get(idx);
+    }
+
+    public NumericNode getNumber(int idx) {
+        return (NumericNode) get(idx);
+    }
+
+    public TextualNode getText(int idx) {
+        return (TextualNode) get(idx);
+    }
+
+    public String getString(int idx) {
+        return getText(idx).getString();
+    }
+
+    public char getChar(int idx) {
+        return getText(idx).getChar();
+    }
+
+    public byte getByte(int idx) {
+        return getNumber(idx).getByte();
+    }
+
+    public short getShort(int idx) {
+        return getNumber(idx).getShort();
+    }
+
+    public int getInt(int idx) {
+        return getNumber(idx).getInt();
+    }
+
+    public long getLong(int idx) {
+        return getNumber(idx).getLong();
+    }
+
+    public float getFloat(int idx) {
+        return getNumber(idx).getFloat();
+    }
+
+    public double getDouble(int idx) {
+        return getNumber(idx).getDouble();
+    }
+
+    public boolean getBoolean(int idx) {
+        return ((SerialBoolean) get(idx)).getValue();
     }
 
     public int length() {
--- Engine-Core/src/main/java/com/github/rmheuer/engine/core/serial2/node/SerialObject.java
+++ Engine-Core/src/main/java/com/github/rmheuer/engine/core/serial2/node/SerialObject.java
@@ -11,6 +11,10 @@ public final class SerialObject extends TraversableNode {
         map = new HashMap<>();
     }
 
+    public boolean isNull(String key) {
+        return get(key) instanceof SerialNull;
+    }
+
     public SerialNode get(String key) {
         SerialNode actual = getActual(key);
         if (actual instanceof SerialRef) {
@@ -23,16 +27,107 @@ public final class SerialObject extends TraversableNode {
         return map.get(key);
     }
 
+    public SerialObject getObject(String key) {
+        return (SerialObject) get(key);
+    }
+
+    public SerialArray getArray(String key) {
+        return (SerialArray) get(key);
+    }
+
+    public NumericNode getNumber(String key) {
+        return (NumericNode) get(key);
+    }
+
+    public TextualNode getText(String key) {
+        return (TextualNode) get(key);
+    }
+
     public String getString(String key) {
-        return ((TextualNode) get(key)).getString();
+        return getText(key).getString();
+    }
+
+    public char getChar(String key) {
+        return getText(key).getChar();
+    }
+
+    public byte getByte(String key) {
+        return getNumber(key).getByte();
+    }
+
+    public short getShort(String key) {
+        return getNumber(key).getShort();
+    }
+
+    public int getInt(String key) {
+        return getNumber(key).getInt();
+    }
+
+    public long getLong(String key) {
+        return getNumber(key).getLong();
+    }
+
+    public float getFloat(String key) {
+        return getNumber(key).getFloat();
+    }
+
+    public double getDouble(String key) {
+        return getNumber(key).getDouble();
+    }
+
+    public boolean getBoolean(String key) {
+        return ((SerialBoolean) get(key)).getValue();
     }
 
     public void put(String key, SerialNode value) {
+        if (value == null)
+            value = new SerialNull();
+
         SerialNode removed = map.put(key, value);
         if (removed != null)
             removed.setParent(null);
         value.setParent(this);
         value.setPathToken(key);
+    }
+
+    public void putByte(String key, byte val) {
+        put(key, new SerialByte(val));
+    }
+
+    public void putShort(String key, short val) {
+        put(key, new SerialShort(val));
+    }
+
+    public void putInt(String key, int val) {
+        put(key, new SerialInt(val));
+    }
+
+    public void putLong(String key, long val) {
+        put(key, new SerialLong(val));
+    }
+
+    public void putFloat(String key, float val) {
+        put(key, new SerialFloat(val));
+    }
+
+    public void putDouble(String key, double val) {
+        put(key, new SerialDouble(val));
+    }
+
+    public void putChar(String key, char val) {
+        put(key, new SerialChar(val));
+    }
+
+    public void putString(String key, String val) {
+        put(key, new SerialString(val));
+    }
+
+    public void putBoolean(String key, boolean val) {
+        put(key, new SerialBoolean(val));
+    }
+
+    public void putReference(String key, SerialNode target) {
+        put(key, new SerialRef(target));
     }
 
     public boolean containsKey(String key) {
