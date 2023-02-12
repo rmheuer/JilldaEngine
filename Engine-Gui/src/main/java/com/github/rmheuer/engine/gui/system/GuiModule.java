65bfb784e778dfcb00642cead85edb6cca1c30e6
Ryan Heuer <63077980+rmheuer@users.noreply.github.com>
2023-02-12 15:57:12 -0600

    Editor: Fix build

--- Engine-Gui/src/main/java/com/github/rmheuer/engine/gui/system/GuiModule.java
+++ Engine-Gui/src/main/java/com/github/rmheuer/engine/gui/system/GuiModule.java
@@ -2,8 +2,17 @@ package com.github.rmheuer.engine.gui.system;
 
 import com.github.rmheuer.engine.core.main.Game;
 import com.github.rmheuer.engine.core.module.GameModule;
+import com.github.rmheuer.engine.render2d.system.Render2DModule;
+
+import java.util.Collection;
+import java.util.Collections;
 
 public final class GuiModule implements GameModule {
+    @Override
+    public Collection<Class<? extends GameModule>> getDependencies() {
+        return Collections.singleton(Render2DModule.class);
+    }
+
     @Override
     public void initializeWorld(Game.WorldBuilder builder) {
         builder.addSystem(GuiInputEventSystem.class);
